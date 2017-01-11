53a3b5ca2fbb28f664e42d31c3c65e9d522d7a5c
zaturnvalley <zacharycmason@gmail.com>
2017-01-11 13:02:00 -0800

    new time vars and commented goals

--- code.js
+++ code.js
@@ -1,2 +1,25 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+
+function renderTime() {
+  // Time Variables
+  var now = new Date();
+  var today = now.toDateString();
+  var time = now.toLocaleTimeString();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  var milliseconds = now.getMilliseconds();
+
+  // Draw Background 
+
+  // Draw Hours
+
+  // Draw Minutes
+
+  // Draw Seconds
+
+  // Date
+
+  // Time
+}
